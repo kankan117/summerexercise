80bdc3b14290f53ae40ff22aed2338933f7d1677
kankan <e10071216@gmail.com>
2024-08-21 15:59:40 +0800

    change version

--- KeelungSightsCrawler/KeelungSightsCrawler/src/main/java/com/example/KeelungSightsCrawler/SightRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.KeelungSightsCrawler;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.List;
-
-public interface SightRepository extends MongoRepository<Sight, String> {
-    List<Sight> findByZone(String zone);
-}
